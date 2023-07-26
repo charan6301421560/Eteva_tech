f493ae38c8ab45ee93690563c27063b4b887864f
charanReddy <charankumarreddy03@gmail.com>
2023-07-26 17:15:13 +0530

    Eteve Tech assignment

---
+++ index.js
@@ -0,0 +1,26 @@
+const List1 = ['Arjun', 'Adwait', 'Swapnil', 'Amit', 'Vishal', 'Adnan'];
+const List2 = ['Adwait', 'Laxman', 'Amit', 'Adnan', 'Nitin', 'Gaurav'];
+
+const finalList1NotInList2 = [];
+for (let i of List1) {
+  if (!List2.includes(i)) {
+    finalList1NotInList2.push(i);
+  }
+}
+console.log(finalList1NotInList2);
+
+const finalList2NotInList1 = [];
+for (let i of List2) {
+    if (!List1.includes(i)) {
+        finalList2NotInList1.push(i);
+    }
+}
+console.log(finalList2NotInList1);
+
+const finalList2IntersectionList1 = [];
+for (let i of List2) {
+    if (List1.includes(i)) {
+    finalList2IntersectionList1.push(i);
+    }
+}
+console.log(finalList2IntersectionList1);
